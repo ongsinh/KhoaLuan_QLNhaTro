2be355ea63cc43e724f00290a8aee6e97fe21f8c
Sinh1705 <sinhteogp@gmail.com>
2024-10-11 22:27:24 +0700

    Add project files.

---
+++ KhoaLuan_QLNhaTro/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using KhoaLuan_QLNhaTro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KhoaLuan_QLNhaTro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
