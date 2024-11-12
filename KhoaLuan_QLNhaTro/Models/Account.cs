1bd12f7ea2b5238087d10166998ab849e6aef922
rikai-sinhov <sinh.ong@rikai.technolog>
2024-11-12 23:57:52 +0700

    new commit

--- KhoaLuan_QLNhaTro/Models/Account.cs
+++ KhoaLuan_QLNhaTro/Models/Account.cs
@@ -12,7 +12,7 @@ namespace KhoaLuan_QLNhaTro.Models
         public string Phone { get; set; }
         public string Email { get; set; }
         public DateTime Dob { get; set; }
-        
+        public string Address { get; set; }
         public string Password { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set;}
