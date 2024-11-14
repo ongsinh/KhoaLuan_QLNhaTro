6ce26f98a9e632acba1bc16393369c534e770f3b
rikai-sinhov <sinh.ong@rikai.technolog>
2024-11-14 11:33:49 +0700

    change sql / model

--- KhoaLuan_QLNhaTro/Models/Account.cs
+++ KhoaLuan_QLNhaTro/Models/Account.cs
@@ -7,17 +7,11 @@ namespace KhoaLuan_QLNhaTro.Models
     {
         [Key]
         public Guid Id { get; set; }
-        public float Name { get; set; }
-        public string CCCD { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public DateTime Dob { get; set; }
-        public string Address { get; set; }
         public string Password { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set;}
+        public DateTime UpdatedAt { get; set; }
         public Guid RoleId { get; set; }
         public virtual Role Role { get; set; }
-        public virtual ICollection<Bill> Bills { get; set; }
+        public virtual User User { get; set; }
     }
 }
