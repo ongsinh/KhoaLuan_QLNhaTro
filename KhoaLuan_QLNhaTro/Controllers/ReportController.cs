cc8bb00f92d92b078563b896ff322a194c8728a8
rikai-sinhov <sinh.ong@rikai.technolog>
2024-11-22 15:19:52 +0700

    fix chossen menu

---
+++ KhoaLuan_QLNhaTro/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KhoaLuan_QLNhaTro.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReportMain()
+        {
+            return View();
+        }
+    }
+}
